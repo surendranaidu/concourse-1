8036a4b03d0f6f2790c2a6611a6e498cfdb36bd6
Rui Yang <ryang@pivotal.io>
2020-04-29 20:52:54 -0400

    atc: log db transaction query
    when --log-db-queries is set to true
    
    Signed-off-by: Rui Yang <ryang@pivotal.io>

--- atc/db/log_conn.go
+++ atc/db/log_conn.go
@@ -21,21 +21,41 @@ type logConn struct {
 	logger lager.Logger
 }
 
+type logDbTx struct {
+	Tx
+
+	logger lager.Logger
+}
+
+func (t *logDbTx) QueryRow(query string, args ...interface{}) squirrel.RowScanner {
+	t.logger.Debug("tx-query-row", lager.Data{"query": strip(query)})
+	return t.Tx.QueryRow(query, args...)
+}
+
+func (c *logConn) Begin() (Tx, error) {
+	tx, err := c.Conn.Begin()
+	if err != nil {
+		return nil, err
+	}
+
+	return &logDbTx{Tx: tx, logger: c.logger}, nil
+}
+
 func (c *logConn) Query(query string, args ...interface{}) (*sql.Rows, error) {
-	c.logger.Debug("query", lager.Data{"query": c.strip(query)})
+	c.logger.Debug("query", lager.Data{"query": strip(query)})
 	return c.Conn.Query(query, args...)
 }
 
 func (c *logConn) QueryRow(query string, args ...interface{}) squirrel.RowScanner {
-	c.logger.Debug("query-row", lager.Data{"query": c.strip(query)})
+	c.logger.Debug("query-row", lager.Data{"query": strip(query)})
 	return c.Conn.QueryRow(query, args...)
 }
 
 func (c *logConn) Exec(query string, args ...interface{}) (sql.Result, error) {
-	c.logger.Debug("exec", lager.Data{"query": c.strip(query)})
+	c.logger.Debug("exec", lager.Data{"query": strip(query)})
 	return c.Conn.Exec(query, args...)
 }
 
-func (c *logConn) strip(query string) string {
+func strip(query string) string {
 	return strings.Join(strings.Fields(query), " ")
 }
