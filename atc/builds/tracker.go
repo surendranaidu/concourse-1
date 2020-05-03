28ea8e203de27515397b9223e385eb914c243930
Alex Suraci <asuraci@pivotal.io>
2020-05-03 16:42:06 -0400

    atc: structure: tracker is component.Runnable
    removed Release method; will be done differently in a later commit.
    (spoiler: by turning the engine into a Drainer type.)
    
    Signed-off-by: Alex Suraci <asuraci@pivotal.io>

--- atc/builds/tracker.go
+++ atc/builds/tracker.go
@@ -1,22 +1,21 @@
 package builds
 
 import (
+	"context"
 	"sync"
 
 	"code.cloudfoundry.org/lager"
+	"code.cloudfoundry.org/lager/lagerctx"
 	"github.com/concourse/concourse/atc/db"
 	"github.com/concourse/concourse/atc/engine"
 	"github.com/concourse/concourse/atc/metric"
 )
 
 func NewTracker(
-	logger lager.Logger,
-
 	buildFactory db.BuildFactory,
 	engine engine.Engine,
 ) *Tracker {
 	return &Tracker{
-		logger:       logger,
 		buildFactory: buildFactory,
 		engine:       engine,
 		running:      &sync.Map{},
@@ -32,15 +31,15 @@ type Tracker struct {
 	running *sync.Map
 }
 
-func (bt *Tracker) Track() error {
-	tLog := bt.logger.Session("track")
+func (bt *Tracker) Run(ctx context.Context) error {
+	logger := lagerctx.FromContext(ctx)
 
-	tLog.Debug("start")
-	defer tLog.Debug("done")
+	logger.Debug("start")
+	defer logger.Debug("done")
 
 	builds, err := bt.buildFactory.GetAllStartedBuilds()
 	if err != nil {
-		tLog.Error("failed-to-lookup-started-builds", err)
+		logger.Error("failed-to-lookup-started-builds", err)
 		return err
 	}
 
@@ -53,7 +52,7 @@ func (bt *Tracker) Track() error {
 				defer metric.BuildsRunning.Dec()
 
 				engineBuild := bt.engine.NewBuild(build)
-				engineBuild.Run(tLog.WithData(lager.Data{
+				engineBuild.Run(logger.Session("run", lager.Data{
 					"build":    build.ID(),
 					"pipeline": build.PipelineName(),
 					"job":      build.JobName(),
@@ -63,12 +62,4 @@ func (bt *Tracker) Track() error {
 	}
 
 	return nil
-}
-
-func (bt *Tracker) Release() {
-	rLog := bt.logger.Session("release")
-	rLog.Debug("start")
-	defer rLog.Debug("done")
-
-	bt.engine.ReleaseAll(rLog)
 }
